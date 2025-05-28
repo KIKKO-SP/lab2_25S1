3b12534d15adf5662c2ae5aed2669a52b4e827be
Kikko <kmt25.24@ichat.sp.edu.sg>
2025-05-28 19:19:32 +0800

    Updated lab 2

--- test_lab2.py
+++ test_lab2.py
@@ -1,20 +1,20 @@
 import lab2 as LAB2
- 
+
 def test_find_min_max():
     datalist = [1,5,3,6,8,2,4]
     expected_result = (1, 8)
     test_result = LAB2.find_min_max(datalist)
     assert (test_result == expected_result)
- 
- 
+
+
 def test_calc_average():
     datalist = [1,5,3,6,8,2,4]
     expected_result = 4.143
     test_result = LAB2.calc_average(datalist)
     test_result = round(test_result, 3)
     assert (test_result == expected_result)
- 
- 
+
+
 def test_calc_median_temperature():
     datalist = [1,5,3,6,8,2,4]
     expected_result = 4.0
